62902f84c31c9b2e783d9e1b340393855af1e4c9
Shree Devi Yadam <shree.yadam@gmail.com>
2021-08-17 12:14:14 -0400

    Add labels for daat values along X-axiz

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
+function maxDataVal(data) {
+  return data.reduce((prev, curr) => curr[1] > prev ? curr[1] : prev, data[0][1]);
+}
 function drawBarChart(data, options, element){
   let $barAxes = $("<div class='bar-chart-axes'></div>");
   $barAxes.css('width', options.width || '1000px');
   $barAxes.css('height', options.height || '500px');
   element.append($barAxes);
   for(let i = 0; i < data.length ; i++) {
+    $('.bar-chart-axes').append(`<div id="dataLabel${i}" class="dataLabel"></div>`);
     const elemId = `bar${i}`;
     const calcMargin = 900 / (data.length * 2);
-    const elemString = `<div id=${elemId} class='bar'></div>`;
-    $('.bar-chart-axes').append(elemString);
+    const elemString = `<div id=${elemId} class='bar'><p id="value${i}" class="data-value">${data[i][1]}</p></div>`;
+    $(`#dataLabel${i}`).append(elemString);
+    $(`#dataLabel${i}`).append('<div class="line"></div>');
+    $(`#dataLabel${i}`).append(`<div id="bar-label${i}" class="label"><p  >${data[i][0]}</p><div>`);
     const elemIDSelector = `#${elemId}`;
     $(elemIDSelector).css("width", `${calcMargin}px`);
-    $(elemIDSelector).css("height", `${(data[i] / Math.max(...data)) * 90}%`);
+    $(elemIDSelector).css("height", `${(data[i][1] / maxDataVal(data)) * 90}%`);
     $(elemIDSelector).css("margin-left", `${calcMargin}px`);
-    $(elemIDSelector).append(`<p class="data-value">${data[i]}</p>`);
-    console.log(element);
+    $(`#bar-label${i}`).css("width", `${calcMargin}px`);
+    $(`#bar-label${i}`).css("margin-left", `${calcMargin}px`);
   }
-  console.log(element);
 }
 
 $(document).ready(function () {
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const data = [['a', 1], ['b', 2], ['c', 3], ['d', 4], ['e', 5], ['f', 6], ['g', 7], ['h', 8], ['i', 9], ['j', 10]];
   const options = {width: '1200px', height: '1200px'};
   const barChartDiv = $('#bar-chart');
   drawBarChart(data, options, barChartDiv);
